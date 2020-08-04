5114c255d3424b65bb9e8bef5c402bbbf804df6e
Darius Ilcevic <darekxlr@gmail.com>
2020-08-04 12:39:50 +0200

    Updates

---
+++ precache-manifest.8f9dee24189770513b23b0bb06dadaf7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "39d8eb2667b87c3b0aa5",
+    "url": "/Menu/static/css/main.9824d148.chunk.css"
+  },
+  {
+    "revision": "39d8eb2667b87c3b0aa5",
+    "url": "/Menu/static/js/main.bf827891.chunk.js"
+  },
+  {
+    "revision": "166c21527d970bbec79f",
+    "url": "/Menu/static/js/runtime~main.de02c97b.js"
+  },
+  {
+    "revision": "e167ca19f56fcc8e8655",
+    "url": "/Menu/static/js/2.b5486003.chunk.js"
+  },
+  {
+    "revision": "572e8f34680941aff7c9d989d2f316f7",
+    "url": "/Menu/static/media/town.572e8f34.jpg"
+  },
+  {
+    "revision": "43886cfd51d251f5a707423e3f9c94a4",
+    "url": "/Menu/static/media/chicago.43886cfd.jpg"
+  },
+  {
+    "revision": "7cda8c9b69113c627cb3a9b8ef89f204",
+    "url": "/Menu/static/media/kuala.7cda8c9b.jpg"
+  },
+  {
+    "revision": "451aac4de9e3eda09b5944a9f039ba35",
+    "url": "/Menu/static/media/paris.451aac4d.jpg"
+  },
+  {
+    "revision": "fe80c94412aaa89779917a5b028b2e0a",
+    "url": "/Menu/index.html"
+  }
+];
